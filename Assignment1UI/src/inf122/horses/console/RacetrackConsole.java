1fc212f2a2078f824dc088d9861340be71eafd61
Alex Thornton <thornton@ics.uci.edu>
2012-01-25 22:43:40 -0800

    The main console loop is now in place, including the use of a "phantom" WELCOME command at startup, as well as STOP when there is no more user input (e.g., when input is redirected in from a file)

--- Assignment1UI/src/inf122/horses/console/RacetrackConsole.java
+++ Assignment1UI/src/inf122/horses/console/RacetrackConsole.java
@@ -7,6 +7,12 @@
 
 package inf122.horses.console;
 
+import inf122.horses.console.commands.StopCommand;
+import inf122.horses.console.commands.WelcomeCommand;
+import inf122.horses.console.parsers.CommandParser;
+import inf122.horses.console.parsers.InvalidCommandException;
+import java.util.Scanner;
+
 
 public class RacetrackConsole
 {
@@ -18,9 +24,35 @@ public class RacetrackConsole
 	
 	public void run()
 	{
+		new WelcomeCommand().execute(state);
 		
+		consoleIn = new Scanner(System.in);
+		CommandParser commandParser = new CommandParser();
+		
+		while (true)
+		{
+			String commandString = consoleIn.nextLine();
+			
+			if (commandString == null)
+			{
+				new StopCommand().execute(state);
+				break;
+			}
+			else
+			{
+				try
+				{
+					commandParser.parse(commandString).execute(state);
+				}
+				catch (InvalidCommandException e)
+				{
+					System.out.println(e.getMessage());
+				}
+			}
+		}
 	}
 	
 	
 	private RacetrackState state;
+	private Scanner consoleIn;
 }
---
+++ Assignment1UI/src/inf122/horses/console/commands/WelcomeCommand.java
@@ -0,0 +1,20 @@
+// WelcomeCommand.java
+//
+// Informatics 122 Winter 2012
+// Assignment #1: Who's Gonna Ride Your Wild Horses?
+//
+// Implements a "phantom" command called WELCOME, which is not supported
+// in the user interface, but is instead used to generate a welcome message
+// on startup.
+
+package inf122.horses.console.commands;
+
+import inf122.horses.console.RacetrackState;
+
+
+public class WelcomeCommand implements Command
+{
+	public void execute(RacetrackState state)
+	{
+	}
+}
